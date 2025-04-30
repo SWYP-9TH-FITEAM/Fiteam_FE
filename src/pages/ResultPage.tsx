b6f40aa8eda9d4b01cb54516e71216908322b9aa
yejinleee <yejin9487@gmail.com>
2025-04-30 23:12:54 +0900

    feat: 테스트-결과

---
+++ src/pages/ResultPage.tsx
@@ -0,0 +1,68 @@
+import LayoutMo from '@/layouts/LayoutMo';
+import xIcon from '@/assets/xIcon.svg';
+
+const ResultPage = () => {
+  return (
+    <LayoutMo bgColor="#F1F2F4">
+      <div className="flex flex-col items-center mb-[45px]">
+        <div className="h-12 flex ml-auto">
+          <button aria-label="홈으로 이동">
+            <img src={xIcon} alt="홈으로" />
+          </button>
+        </div>
+        <div className="w-full h-[487px] bg-gray-300">img</div>
+        <p className="ml-[26px] mr-3 text-[#111]">
+          이미지를 꾹 누르면 저장이 돼요
+        </p>
+        <div className="w-[335px] h-[310px] shrink-0 bg-white mt-3.5 rounded-lg shadow-sm">
+          성향카드
+        </div>
+        <div className="w-full flex gap-[15px] mt-3.5">
+          <div className="flex-1 h-40 shrink-0 bg-white rounded-lg shadow-sm">
+            잘
+          </div>
+          <div className="flex-1 h-40 shrink-0 bg-white rounded-lg shadow-sm">
+            않
+          </div>
+        </div>
+        <div className="mt-[35px] mb-[31px]">
+          <p className="text-lg font-medium">친구에게 공유하기</p>
+          <div className="flex gap-4 mt-4 mb-[30px]">
+            {[1, 2, 3, 4].map(index => (
+              <div
+                key={index}
+                className="w-[38px] h-[38px] shrink-0 rounded-[50%] bg-yellow-200"
+                role="button"
+                tabIndex={0}
+                aria-label={`공유 옵션 ${index}`}
+              />
+            ))}
+          </div>
+          <a
+            href="naver.com"
+            className="text-primary hover:underline"
+            aria-label="홈으로 이동"
+          >
+            홈으로
+          </a>
+        </div>
+        <div className="w-full flex flex-col gap-2">
+          <button
+            className="flex h-[54px] justify-center items-center gap-2.5 bg-white self-stretch px-[93px] py-[13px] rounded-[10px] shadow-sm hover:bg-gray-50 transition-colors"
+            aria-label="모든 결과 유형보기"
+          >
+            모든 결과 유형보기
+          </button>
+          <button
+            className="flex h-[54px] justify-center items-center gap-2.5 self-stretch bg-[#5F4AFF] px-7 py-[13px] rounded-[10px] text-white hover:bg-[#4a3ad1] transition-colors"
+            aria-label="내 캐릭터 토대로 프로필카드 만들기"
+          >
+            내 캐릭터 토대로 프로필카드 만들기
+          </button>
+        </div>
+      </div>
+    </LayoutMo>
+  );
+};
+
+export default ResultPage;
