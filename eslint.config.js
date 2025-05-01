6e4a05e119bedca2a573d17efa38318272ea1ada
yejinleee <yejin9487@gmail.com>
2025-05-01 16:10:19 +0900

    chore: update ESLint and Tailwind configurations
    - Disabled specific jsx-a11y rules related to click events and non-interactive elements for flexibility in component interactions.
    - Standardized quotation marks in tailwind.config.js for consistency.

--- eslint.config.js
+++ eslint.config.js
@@ -26,6 +26,9 @@ export default tseslint.config(
         {allowConstantExport: true},
       ],
       ...jsxA11y.configs.recommended.rules,
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/no-noninteractive-element-interactions': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
     },
   },
 );
