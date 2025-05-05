72bec343667ab4af31252a9da95bb15f380801c9
yejinleee <yejin9487@gmail.com>
2025-05-05 21:30:41 +0900

    feat: add background image and update MainPage styling

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,20 +1,34 @@
 import {Button} from '@/components/ui/button';
 import {useNavigate} from 'react-router-dom';
+import logo from '@/assets/images/logo.png';
+import backgroundImage from '@/assets/images/background.png';
 
 const MainPage = () => {
   const navigate = useNavigate();
 
-  // 임의 배경구분 색 bg-amber-1000
   return (
-    <div className="flex flex-col justify-between px-[60px] py-[30px] bg-amber-100 min-h-screen">
-      <div className="text-center">
-        <h1 className="text-3xl font-bold mb-2">Fiteam</h1>
-        <p className="text-base text-gray-600">나만의 팀을 만드는 특별함</p>
+    <div
+      className="flex flex-col justify-between px-[60px] py-[40px] min-h-screen"
+      style={{
+        background: `linear-gradient(
+          0deg,
+          rgba(255, 255, 255, 0.3) 0%,
+          rgba(255, 255, 255, 0.3) 100%
+        ),
+        linear-gradient(187deg, #fff 0.79%, rgba(255, 255, 255, 0) 69.96%),
+        url(${backgroundImage}) lightgray 50% / cover no-repeat`,
+      }}
+    >
+      <div className="flex flex-col items-center gap-1.5">
+        <img src={logo} alt="Fiteam" className="w-[126px]" />
+        <p className="text-[#585858] text-center text-base font-semibold leading-7">
+          나만의 팀을 만드는 특별함
+        </p>
       </div>
       <div className="w-full h-[370px] bg-gray-200 " />
       <Button
         onClick={() => navigate('/onboarding')}
-        className="w-full flex  h-[54px] justify-center items-center gap-2.5 shrink-0 px-[132px] py-[13px] rounded-[10px] bg-[#E9E9E9] text-black text-xl font-medium hover:bg-[#c9c9c9]"
+        className="w-full flex  h-[54px] justify-center items-center gap-2.5 shrink-0 px-[132px] py-[13px] rounded-[10px] bg-primary text-white text-xl font-medium"
       >
         시작하기
       </Button>
