aba6ee406b195e97c8659b5099289ef4c472522a
yejinleee <yejin9487@gmail.com>
2025-05-05 10:36:51 +0900

    feat: add chat icons and update bottom bar layout to include chat functionality

---
+++ src/assets/bottomIcon/index.ts
@@ -0,0 +1,23 @@
+import homeSelected from './home-selected.svg';
+import home from './home.svg';
+import myPageSelected from './mypage-selected.svg';
+import myPage from './mypage.svg';
+import profileSelected from './profile-selected.svg';
+import profile from './profile.svg';
+import teamBuildingSelected from './teambuilding-selected.svg';
+import teamBuilding from './teambuilding.svg';
+import chat from './chat.svg';
+import chatSelected from './chat-selected.svg';
+
+export {
+  homeSelected,
+  home,
+  profileSelected,
+  profile,
+  teamBuildingSelected,
+  teamBuilding,
+  chat,
+  chatSelected,
+  myPageSelected,
+  myPage,
+};
