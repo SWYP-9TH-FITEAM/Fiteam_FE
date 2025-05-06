8b43dc8665a3446283a59b6ae254cbac9140e80a
Dohyeong Kim <kdh@m4nd4r1n.me>
2025-05-06 22:48:07 +0900

    feat: implement authentication token management with Jotai and local storage

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,5 +1,22 @@
 import ky from 'ky';
+import {tokenAtom} from '../model/auth';
+import {getDefaultStore} from 'jotai';
 
 export const api = ky.create({
   prefixUrl: import.meta.env.VITE_BE_API,
+});
+
+export const apiWithAuth = api.extend({
+  hooks: {
+    beforeRequest: [
+      async request => {
+        const defaultStore = getDefaultStore();
+        const token = defaultStore.get(tokenAtom);
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`);
+        }
+      },
+    ],
+  },
 });
