0e6a584ff5773ad58f18984d52c2b73a5ec953f6
Dohyeong Kim <kdh@m4nd4r1n.me>
2025-05-10 14:23:07 +0900

    feat: update MyPage component to include navigation links for profile, settings, and announcements

--- src/pages/my-page/ui/MyPage.tsx
+++ src/pages/my-page/ui/MyPage.tsx
@@ -1,6 +1,7 @@
 import {LayoutBottomBar} from '@/layouts/LayoutBottomBar';
 import {ChevronRight} from 'lucide-react';
 import * as React from 'react';
+import {Link} from 'react-router-dom';
 
 export const MyPage: React.FC = () => {
   return (
@@ -19,24 +20,36 @@ export const MyPage: React.FC = () => {
         <div className="w-14 h-14 rounded-full bg-white"></div>
         <div className="flex flex-col items-start">
           <span className="text-xl font-medium tracking-[-0.5px]">아무개</span>
-          <span className="text-sm font-medium tracking-[-0.5px] text-[#585858]">
+          <Link
+            to="/profile"
+            className="text-sm font-medium tracking-[-0.5px] text-[#585858]"
+          >
             기본 정보 보기
-          </span>
+          </Link>
         </div>
       </div>
 
       <div className="rounded-t-[30px] bg-white shadow-[0px_-1px_2px_0px_#CECBE8] px-5 pt-6 mt-6 flex-1 divide-y divide-[#F1F2F4]">
-        <div className="py-3 h-16 text-lg tracking-[-0.45px] text-left flex items-center justify-between">
+        <Link
+          to="/profile"
+          className="py-3 h-16 text-lg tracking-[-0.45px] text-left flex items-center justify-between"
+        >
           내 프로필 카드
-        </div>
-        <div className="py-3 h-16 text-lg tracking-[-0.45px] text-left flex items-center justify-between">
+        </Link>
+        <Link
+          to="/settings"
+          className="py-3 h-16 text-lg tracking-[-0.45px] text-left flex items-center justify-between"
+        >
           설정
-        </div>
-        <div className="py-3 h-16 text-lg tracking-[-0.45px] flex items-center justify-between">
+        </Link>
+        <Link
+          to="/announcements"
+          className="py-3 h-16 text-lg tracking-[-0.45px] flex items-center justify-between"
+        >
           <span>공지사항/이벤트</span>
           <ChevronRight className="stroke-[1.5]" />
-        </div>
-        <div className="py-3 h-16 text-lg tracking-[-0.45px] flex items-center justify-between">
+        </Link>
+        <div className="py-3 h-16 font-medium text-lg tracking-[-0.45px] flex items-center justify-between">
           <span>웹 정보</span>
           <span>v 1.0</span>
         </div>
