d1fdaee25045f3d7d8d42f1934708b9f189c0d65
Dohyeong Kim <kdh@m4nd4r1n.me>
2025-05-10 14:22:48 +0900

    feat: add hideBottomBar prop to LayoutBottomBar for conditional rendering

--- src/layouts/LayoutBottomBar.tsx
+++ src/layouts/LayoutBottomBar.tsx
@@ -11,6 +11,7 @@ type LayoutBottomBarProps = {
     wrapper?: string;
     scrollableArea?: string;
   };
+  hideBottomBar?: boolean;
 };
 
 const BottomBar = () => {
@@ -88,6 +89,7 @@ export const LayoutBottomBar = ({
   children,
   header,
   classNames,
+  hideBottomBar = false,
 }: LayoutBottomBarProps) => {
   return (
     <div
@@ -108,7 +110,7 @@ export const LayoutBottomBar = ({
         {children}
       </div>
 
-      <BottomBar />
+      {!hideBottomBar && <BottomBar />}
     </div>
   );
 };
