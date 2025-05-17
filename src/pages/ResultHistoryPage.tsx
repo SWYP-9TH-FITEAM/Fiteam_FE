cbf772b43f19b594a452a19710513425ba4deffe
yejinleee <yejin9487@gmail.com>
2025-05-17 19:22:33 +0900

    feat: 히스토리 화면 api

--- src/pages/ResultHistoryPage.tsx
+++ src/pages/ResultHistoryPage.tsx
@@ -6,21 +6,6 @@ interface ResultHistoryPageProps {
   onClose: () => void;
 }
 
-const dummyHistory = [
-  {
-    id: 1,
-    date: '2025.05.08',
-    title: '반짝이는 아이디어 조율요정',
-    image: '/src/assets/images/robot.png',
-  },
-  {
-    id: 2,
-    date: '2025.05.02',
-    title: '자유를 찾아 떠나는 방랑자',
-    image: '/src/assets/images/robot.png',
-  },
-];
-
 const ResultHistoryPage = ({onClose}: ResultHistoryPageProps) => {
   const {data: cardIdsData} = useQuery(userQueries.cardIds());
 
@@ -34,21 +19,21 @@ const ResultHistoryPage = ({onClose}: ResultHistoryPageProps) => {
       bgColor="white"
     >
       <div className="flex flex-col gap-2 px-2">
-        {dummyHistory.map(item => (
+        {cardIdsData?.map((item, index) => (
           <div
-            key={item.id}
+            key={`${item.code}-${index}`}
             className="flex items-center py-4 border-b border-[#E5E5E5] last:border-b-0"
           >
             <img
-              src={item.image}
-              alt="캐릭터"
+              src={item.imgUrl}
+              alt={item.name}
               className="w-[80px] h-[80px] rounded-[18px] bg-[#F7F7FA] object-contain mr-4"
             />
             <div className="flex flex-col flex-1">
               <div className="flex-1 text-left">
-                <div className="text-xs text-[#BDBDBD] mb-1">{item.date}</div>
+                <div className="text-xs text-[#BDBDBD] mb-1">{item.name}</div>
                 <div className="text-lg font-medium text-[#111] truncate mb-2">
-                  {item.title}
+                  {item.name}
                 </div>
               </div>
               <button
