c15c21b2094c2edbda42a18c2c5891654cae68d9
yejinleee <yejin9487@gmail.com>
2025-05-17 19:39:52 +0900

    feat: 코드리뷰 반영 개선

--- src/pages/ResultHistoryPage.tsx
+++ src/pages/ResultHistoryPage.tsx
@@ -31,15 +31,13 @@ const ResultHistoryPage = ({onClose}: ResultHistoryPageProps) => {
             />
             <div className="flex flex-col flex-1">
               <div className="flex-1 text-left">
-                <div className="text-xs text-[#BDBDBD] mb-1">{item.name}</div>
+                {/* 테스트일 임시 제거 */}
+                {/* <div className="text-xs text-[#BDBDBD] mb-1">{item.name}</div> */}
                 <div className="text-lg font-medium text-[#111] truncate mb-2">
                   {item.name}
                 </div>
               </div>
-              <button
-                className="ml-auto px-[9px] py-1 w-[70px] h-[28px] border border-gray-2 rounded-[4px] text-gray-5 text-[13px] font-medium bg-white"
-                disabled
-              >
+              <button className="ml-auto px-[9px] py-1 w-[70px] h-[28px] border border-gray-2 rounded-[4px] text-gray-5 text-[13px] font-medium bg-white">
                 결과 보기
               </button>
             </div>
