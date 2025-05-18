eeeb0fd1c9fdd2deca9004f988d07b7d8276b52b
Dohyeong Kim <kdh@m4nd4r1n.me>
2025-05-18 18:47:15 +0900

    feat: enhance Prettier configuration with new plugins and import sorting
    - Added '@ianvs/prettier-plugin-sort-imports' and 'prettier-plugin-tailwindcss' to the Prettier configuration for improved import sorting and Tailwind CSS support.
    - Defined custom import order and parser plugins to streamline code formatting.
    - Updated package dependencies in package.json and package-lock.json to include the new plugins.

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,24 @@
 module.exports = {
   ...require('gts/.prettierrc.json'),
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  importOrder: [
+    '<TYPES>^(node:)',
+    '<TYPES>',
+    '<TYPES>^[.]',
+    '',
+    '^(react/(.*)$)|^(react$)|^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^(@/(.*)$)',
+    '^[./]',
+    '',
+    '^(?!.*[.]css$)[./].*$',
+    '.css$',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.0.0',
+  tailwindFunctions: ['cn', 'cva'],
 };
